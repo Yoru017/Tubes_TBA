e406805c823cf32be4fa0c99959d587b8e4d521e
yoru <fikri36987@gmail.com>
2024-06-14 16:26:46 +0700

    bnerin yg no 1

--- html_parser.py
+++ html_parser.py
@@ -1,127 +1,79 @@
-import sys
+import sys, recog
 
-def alpha(char):
-    return ('a' <= char <= 'z') or ('A' <= char <= 'Z') or ('0' <= char <= '9')
+# Tokenizer function
+def tokenize(teks):
+    tokens = []
+    pos = 0
+    while pos < len(teks):
+        token = cek_dfa(teks[pos:])
+        if token:
+            tokens.append(token)
+            pos = pos + len(token)
+        else:
+            pos += 1
+    return tokens
 
-def start_state(char):
-    if char == "<":
-        return "CHECK", ""
-    return "END", ""
-
-def check_state(char, curr_tag):
-    if char == "/":
-        curr_tag += char
-        return "TAG_CLOSE", curr_tag
-    elif alpha(char):
-        curr_tag += char
-        return "TAG_NAME", curr_tag
-    return "END", curr_tag
-
-def tag_name_state(char, curr_tag):
-    if alpha(char):
-        curr_tag += char
-        return "TAG_NAME", curr_tag
-    elif char == ">":
-        return "END", curr_tag
-    return "END", curr_tag
-
-def tag_close_state(char, curr_tag):
-    if alpha(char):
-        curr_tag += char
-        return "TAG_CLOSE", curr_tag
-    elif char == ">":
-        return "END", curr_tag
-    return "END", curr_tag
-
-def recog(token):
-    curr_state = "START"
-    curr_tag = ""
-    acc_tags = ['html', 'head', 'body', 'title', 'h1', 'p', 'nav']
-
-    for char in token:
-        if curr_state == "START":
-            curr_state, curr_tag = start_state(char)
-        elif curr_state == "CHECK":
-            curr_state, curr_tag = check_state(char, curr_tag)
-        elif curr_state == "TAG_NAME":
-            curr_state, curr_tag = tag_name_state(char, curr_tag)
-        elif curr_state == "TAG_CLOSE":
-            curr_state, curr_tag = tag_close_state(char, curr_tag)
-        elif curr_state == "END":
-            break
-
-    if curr_state == "END" and (curr_tag in acc_tags or (len(curr_tag) > 1 and curr_tag[0] == "/" and curr_tag[1:] in acc_tags)):
-        return curr_tag
-
+def cek_dfa(text):
+    for length in range(1, len(text) + 1):
+        if recog.recog(text[:length]) == "ACCEPTED":
+            return text[:length]
     return None
 
-def closetag(html, start):
-    pos = start
-    while pos < len(html):
-        if html[pos] == ">":
-            return pos
-        pos += 1
-    return -1
-
-def parse_html(html):
-    stack = ["#"]
-    pos = 0
-    head_found = False
-    body_found = False
-    html_found = False
-    title_found = False
-
-    while pos < len(html):
-        if html[pos] == "<":
-            end_pos = closetag(html, pos)
-            if end_pos == -1:
-                return "Rejected"
-            tag = html[pos:end_pos + 1]
-            tag_name = recog(tag)
-            if tag_name is None:
-                return "Rejected"
-            if tag_name == "html":
-                html_found = True
-            if tag_name == "head":
-                if body_found:
-                    return "Rejected"  # <head> tidak boleh muncul setelah <body>
-                head_found = True
-            if tag_name == "body":
-                if not html_found:
-                    return "Rejected" #body harus ada di dalam html
-                elif stack[-1] == "head":
-                    if head_found:
-                        return "Rejected" # <body> harus ada di dalam <html> dan gaboleh diantara tag head
-                body_found = True
-            if tag_name == "title":
-                if not head_found or head_found and body_found:
-                    return "Rejected"  # <title> harus ada di dalam <head> dan sebelum <body>
-                title_found = True
-            if not (len(tag_name) > 1 and tag_name[0] == "/"):
-                stack.append(tag_name)
+def parser(input,parse_table):
+    stack = []
+    stack.append('#')
+    stack.append('S')
+    input.append('EOS')
+    i = 0
+    while stack[-1]!='#': #looping hingga isi stack teratas adalah #
+        read = input[i] #LL(1)
+        top = stack.pop()
+        if top in parse_table: #jika top daripada stack berupa non terminal
+            if read in parse_table[top]: #kalau 'read/symbol' ada di parse table 
+                produksi = parse_table[top][read] # 
+                if produksi !='':
+                    stack.extend(reversed(produksi.split())) #push hasil produksi ke stack secara terbalik
             else:
-                if len(stack) == 0 or stack[-1] != tag_name[1:]:
-                    return "Rejected"
-                if stack[-1] == "head":
-                    if not title_found:
-                        return "Rejected"
-                stack.pop()
+                return False
+        else: #jika top daripada stack berupa terminal
+            if top == read:
+                i= i+1 #input maju
+            else:
+                return False
+    top = stack.pop()
+    return stack == [] and top == "#"
 
-            pos = end_pos
-        pos += 1
-
-    if html_found and len(stack) == 1 and stack[-1] == "#":
-        stack.pop()  # Pop the # symbol
-        # Accept if all necessary tags are found and properly nested
-        if head_found and body_found and title_found:
-            return "Accepted"
-        if not head_found and not body_found and not title_found:
-            return "Accepted"
-        # Accept if all necessary tags are found and properly nested
-        if head_found or body_found:
-            return "Accepted"
-    
-    return "Rejected"
+#mendefinisikan grammar dan parser table
+grammar = {
+    'S': ['<html> A </html>'],
+    'A': ['<head> D </head> B','B',''],
+    'B': ['<body> C </body>', ''],
+    'C': ['<h1> </h1> C', '<p> </p> C', ''],
+    'D': ['<title> </title>', ''],
+}
+parse_table = {
+    'S': {
+        '<html>': '<html> A </html>',
+    },
+    'A': {
+        '<head>': '<head> D </head> B',
+        '<body>': 'B',
+        '</html>': '',
+    },
+    'B': {
+        '<body>': '<body> C </body>',
+        '</html>': ''
+    },
+    'C': {
+        '<h1>': '<h1> </h1> C', 
+        '<p>': '<p> </p> C',
+        '</body>': '',
+    },
+    'D': {
+       '<title>': '<title> </title>',
+       '</head>': ''
+    }
+}
 
 def main():
     if len(sys.argv) != 2:
@@ -137,13 +89,12 @@ def main():
         print(f"File not found: {filename}")
         return
 
-    # Print the content of the HTML file
-    print("Isi file HTML:")
-    print(html_content)
-    print()
+    # Menampilkan isi tag yang ada pada file HTML
+    print("Isi file tag pada file HTML:")
+    token_html = tokenize(html_content)
+    print(token_html)
 
-    result = parse_html(html_content)
-    print(f"Hasil: {result}")
+    print(parser(token_html,parse_table))
 
 if __name__ == "__main__":
     main()
--- recog.py
+++ recog.py
@@ -1,10 +1,10 @@
 def alpha(char):
-    return ('a' <= char <= 'z') or ('A' <= char <= 'Z') or ('0' <= char <='9')
+    return ('a' <= char <= 'z') or ('A' <= char <= 'Z') or ('0' <= char <= '9')
 
 def start_state(char):
     if char == "<":
         return "CHECK", ""
-    return "END", ""
+    return "INVALID", ""
 
 def check_state(char, curr_tag):
     if char == "/":
@@ -13,7 +13,7 @@ def check_state(char, curr_tag):
     elif alpha(char):
         curr_tag += char
         return "TAG_NAME", curr_tag
-    return "END", curr_tag
+    return "INVALID", curr_tag
 
 def tag_name_state(char, curr_tag):
     if alpha(char):
@@ -21,7 +21,7 @@ def tag_name_state(char, curr_tag):
         return "TAG_NAME", curr_tag
     elif char == ">":
         return "END", curr_tag
-    return "END", curr_tag
+    return "INVALID", curr_tag
 
 def tag_close_state(char, curr_tag):
     if alpha(char):
@@ -29,12 +29,15 @@ def tag_close_state(char, curr_tag):
         return "TAG_CLOSE", curr_tag
     elif char == ">":
         return "END", curr_tag
-    return "END", curr_tag
+    return "INVALID", curr_tag
+
+def end_state(char, curr_tag):
+    return "INVALID", curr_tag
 
 def recog(token):
     curr_state = "START"
     curr_tag = ""
-    acc_tags = ['html', 'head', 'body', 'title', 'h1', 'p', 'img']
+    acc_tags = ['html', 'head', 'body', 'title', 'h2', 'p', 'img']
 
     for char in token:
         if curr_state == "START":
@@ -46,6 +49,8 @@ def recog(token):
         elif curr_state == "TAG_CLOSE":
             curr_state, curr_tag = tag_close_state(char, curr_tag)
         elif curr_state == "END":
+            curr_state, curr_tag = end_state(char, curr_tag)
+        elif curr_state == "INVALID":
             break
 
     if curr_state == "END" and (curr_tag in acc_tags or (len(curr_tag) > 1 and curr_tag[0] == "/" and curr_tag[1:] in acc_tags)):
